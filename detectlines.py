835e9d476fd3bf46747daa3bcffbe1c1d98ab7c4
Jarrell Waggoner <jwaggoner@groupon.com>
2013-10-23 21:48:20 -0500

    about to change this file

--- detectlines.py
+++ detectlines.py
@@ -2,20 +2,21 @@ from skimage.viewer import ImageViewer
 from skimage.data import imread
 from skimage.filter import canny
 from skimage.color import rgb2grey
+from skimage.morphology import remove_small_objects
 import numpy as np
 
 im = imread('hancock.png')
 im_grey = rgb2grey(im)
 
-viewer = ImageViewer(im)
-viewer.show()
+viewer = ImageViewer(im).show()
 
 edges = canny(im_grey, sigma=7, low_threshold=0.1, high_threshold=0.2)
 
+ImageViewer(edges).show()
+
 filtered = remove_small_objects(edges, 500, connectivity=2)
 
-viewer = ImageViewer(filtered)
-viewer.show()
+ImageViewer(filtered).show()
 
 red = im_grey.copy()
 red[edges > 0] = 1
